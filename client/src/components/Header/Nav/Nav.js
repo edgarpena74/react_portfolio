bace3176ca78a1a870b0f6c44ddb0c6af7016507
Edgar Pena <edgarpena0740@gmail.com>
2021-04-01 15:31:51 -0700

    test push for heroku deployment

--- client/src/components/Header/Nav/Nav.js
+++ client/src/components/Header/Nav/Nav.js
@@ -2,7 +2,7 @@ import React from "react";
 import Navbar from "react-bootstrap/Navbar";
 import Nav from "react-bootstrap/Nav";
 import "./Nav.css";
-
+//test
 const Navigation = () => {
   return (
     <Navbar>
