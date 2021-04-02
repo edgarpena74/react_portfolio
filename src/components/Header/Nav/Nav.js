6462fbc51e7a68c83f4f9c96f953f42a7cb9e832
Edgar Pena <edgarpena0740@gmail.com>
2021-04-01 17:43:38 -0700

    working on mobile optimization

--- src/components/Header/Nav/Nav.js
+++ src/components/Header/Nav/Nav.js
@@ -1,30 +1,33 @@
 import React from "react";
 import Navbar from "react-bootstrap/Navbar";
+import Container from "react-bootstrap/Container";
 import Nav from "react-bootstrap/Nav";
 import "./Nav.css";
 
 const Navigation = () => {
   return (
-    <Navbar sm="12">
-      <Navbar.Brand className="navBrand" href="/">
-        <h3>Edgar Peña</h3>
-      </Navbar.Brand>
-      <Navbar className="justify-content-end container-fluid">
-        <Nav.Item>
-          <Nav.Link href="/biography">Biography</Nav.Link>
-        </Nav.Item>
-        <Nav.Item>
-          <Nav.Link href="/portfolio" className="port">
-            Portfolio
-          </Nav.Link>
-        </Nav.Item>
-        <Nav.Item>
-          <Nav.Link href="/resume" className="resume">
-            Resume
-          </Nav.Link>
-        </Nav.Item>
+    <Container className="navContainer">
+      <Navbar className="navbar" sm="12">
+        <Navbar.Brand className="navBrand" href="/" sm="5">
+          <h3>Edgar Peña</h3>
+        </Navbar.Brand>
+        <Navbar className="justify-content-end container-fluid" sm="7">
+          <Nav.Item className="navItem">
+            <Nav.Link href="/biography">Biography</Nav.Link>
+          </Nav.Item>
+          <Nav.Item className="navItem">
+            <Nav.Link href="/portfolio" className="port">
+              Portfolio
+            </Nav.Link>
+          </Nav.Item>
+          <Nav.Item className="navItem">
+            <Nav.Link href="/resume" className="resume">
+              Resume
+            </Nav.Link>
+          </Nav.Item>
+        </Navbar>
       </Navbar>
-    </Navbar>
+    </Container>
   );
 };
 
