adc8cdf840e73ab66958f94be7908cda2946e545
Edgar Pena <edgarpena0740@gmail.com>
2021-06-16 12:41:19 -0700

    correct error with bootstrap import // updated build

--- src/components/Main/Resume/Resume.js
+++ src/components/Main/Resume/Resume.js
@@ -2,7 +2,7 @@ import React from "react";
 
 import Container from "react-bootstrap/Container";
 import Row from "react-bootstrap/Row";
-import Col from "react-bootstrap/COl";
+import Col from "react-bootstrap/Col";
 import Image from "react-bootstrap/Image";
 // Images
 import projectThree from "../../Pics/proj3EV.png";
